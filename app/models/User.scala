d2cbfcc4be5abbd9244a87230176d09099f5d7e1
YoshinoriN <nayo-msoft@outlook.jp>
2017-09-09 01:15:56 +0900

    Integrate UserDAO and UserDAOImpl to one file

--- app/models/User.scala
+++ app/models/User.scala
@@ -1,6 +1,48 @@
 package app.models
 
+import scala.collection.mutable
+import scala.concurrent.Future
+
 import com.mohiva.play.silhouette.api.{ Identity, LoginInfo }
+import app.models.UserIdentify
+import app.models.UserDAOImpl._
+
+/**
+  * The companion object.
+  */
+object UserDAOImpl {
+
+  /**
+    * The list of users.
+    */
+  val users: mutable.HashMap[String, UserIdentify] = mutable.HashMap()
+}
+
+/**
+  * Give access to the user object.
+  */
+class UserDAOImpl extends UserDAO {
+
+  def find(loginInfo: LoginInfo) = Future.successful(
+    users.find { case (_, user) => user.loginInfo == loginInfo }.map(_._2)
+  )
+
+  def find(userID: String) = Future.successful(users.get(userID))
+
+  def save(user: UserIdentify) = {
+    users += (user.userID -> user)
+    Future.successful(user)
+  }
+}
+
+trait UserDAO {
+
+  def find(loginInfo: LoginInfo): Future[Option[UserIdentify]]
+
+  def find(userID: String): Future[Option[UserIdentify]]
+
+  def save(user: UserIdentify): Future[UserIdentify]
+}
 
 case class UserIdentify(
   userID: String,
