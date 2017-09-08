63a8f3cf28758eef285182c1779fbab92c5324e3
YoshinoriN <nayo-msoft@outlook.jp>
2017-09-09 01:30:03 +0900

    Create SilhouetteModule

---
+++ app/modules/SilhouetteModule.scala
@@ -0,0 +1,219 @@
+package modules
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.Configuration
+import play.api.libs.openid.OpenIdClient
+import play.api.libs.ws.WSClient
+import play.api.mvc.CookieHeaderEncoding
+
+import com.google.inject.name.Named
+import com.google.inject.{ AbstractModule, Provides }
+import com.mohiva.play.silhouette.api.actions.{ SecuredErrorHandler, UnsecuredErrorHandler }
+import com.mohiva.play.silhouette.api.crypto._
+import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
+import com.mohiva.play.silhouette.api.services._
+import com.mohiva.play.silhouette.api.util._
+import com.mohiva.play.silhouette.api.{ Environment, EventBus, Silhouette, SilhouetteProvider }
+import com.mohiva.play.silhouette.crypto.{ JcaCrypter, JcaCrypterSettings, JcaSigner, JcaSignerSettings }
+import com.mohiva.play.silhouette.impl.authenticators._
+import com.mohiva.play.silhouette.impl.providers._
+import com.mohiva.play.silhouette.impl.providers.openid.services.PlayOpenIDService
+import com.mohiva.play.silhouette.impl.providers.state.{ CsrfStateItemHandler, CsrfStateSettings }
+import com.mohiva.play.silhouette.impl.services._
+import com.mohiva.play.silhouette.impl.util._
+import com.mohiva.play.silhouette.password.{ BCryptPasswordHasher, BCryptSha256PasswordHasher }
+import com.mohiva.play.silhouette.persistence.daos.{ DelegableAuthInfoDAO, InMemoryAuthInfoDAO }
+import com.mohiva.play.silhouette.persistence.repositories.DelegableAuthInfoRepository
+import net.ceedubs.ficus.Ficus._
+import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+import net.codingwell.scalaguice.ScalaModule
+
+import utils.auth.{ CustomSecuredErrorHandler, CustomUnsecuredErrorHandler, DefaultEnv }
+import app.models.{ UserDAO, UserDAOImpl, LDAPAuth }
+import app.services.{ UserService, UserServiceImpl }
+
+/**
+ * The Guice module which wires all Silhouette dependencies.
+ */
+class SilhouetteModule extends AbstractModule with ScalaModule {
+
+  /**
+   * Configures the module.
+   */
+  def configure() {
+    bind[Silhouette[DefaultEnv]].to[SilhouetteProvider[DefaultEnv]]
+    bind[UnsecuredErrorHandler].to[CustomUnsecuredErrorHandler]
+    bind[SecuredErrorHandler].to[CustomSecuredErrorHandler]
+    bind[UserService].to[UserServiceImpl]
+    bind[UserDAO].to[UserDAOImpl]
+    bind[CacheLayer].to[PlayCacheLayer]
+    bind[IDGenerator].toInstance(new SecureRandomIDGenerator())
+    bind[FingerprintGenerator].toInstance(new DefaultFingerprintGenerator(false))
+    bind[EventBus].toInstance(EventBus())
+    bind[Clock].toInstance(Clock())
+
+    // Replace this with the bindings to your concrete DAOs
+    bind[DelegableAuthInfoDAO[PasswordInfo]].to[LDAPAuth]
+  }
+
+  /**
+   * Provides the HTTP layer implementation.
+   *
+   * @param client Play's WS client.
+   * @return The HTTP layer implementation.
+   */
+  @Provides
+  def provideHTTPLayer(client: WSClient): HTTPLayer = new PlayHTTPLayer(client)
+
+  /**
+   * Provides the Silhouette environment.
+   *
+   * @param userService The user service implementation.
+   * @param authenticatorService The authentication service implementation.
+   * @param eventBus The event bus instance.
+   * @return The Silhouette environment.
+   */
+  @Provides
+  def provideEnvironment(
+    userService: UserService,
+    authenticatorService: AuthenticatorService[CookieAuthenticator],
+    eventBus: EventBus): Environment[DefaultEnv] = {
+
+    Environment[DefaultEnv](
+      userService,
+      authenticatorService,
+      Seq(),
+      eventBus
+    )
+  }
+
+  /**
+   * Provides the signer for the CSRF state item handler.
+   *
+   * @param configuration The Play configuration.
+   * @return The signer for the CSRF state item handler.
+   */
+  @Provides @Named("csrf-state-item-signer")
+  def provideCSRFStateItemSigner(configuration: Configuration): Signer = {
+    val config = configuration.underlying.as[JcaSignerSettings]("silhouette.csrfStateItemHandler.signer")
+
+    new JcaSigner(config)
+  }
+
+  /**
+   * Provides the signer for the authenticator.
+   *
+   * @param configuration The Play configuration.
+   * @return The signer for the authenticator.
+   */
+  @Provides @Named("authenticator-signer")
+  def provideAuthenticatorSigner(configuration: Configuration): Signer = {
+    val config = configuration.underlying.as[JcaSignerSettings]("silhouette.authenticator.signer")
+
+    new JcaSigner(config)
+  }
+
+  /**
+   * Provides the crypter for the authenticator.
+   *
+   * @param configuration The Play configuration.
+   * @return The crypter for the authenticator.
+   */
+  @Provides @Named("authenticator-crypter")
+  def provideAuthenticatorCrypter(configuration: Configuration): Crypter = {
+    val config = configuration.underlying.as[JcaCrypterSettings]("silhouette.authenticator.crypter")
+
+    new JcaCrypter(config)
+  }
+
+  /**
+   * Provides the auth info repository.
+   *
+   * @param passwordInfoDAO The implementation of the delegable password auth info DAO.
+   * @return The auth info repository instance.
+   */
+  @Provides
+  def provideAuthInfoRepository(
+    passwordInfoDAO: DelegableAuthInfoDAO[PasswordInfo]): AuthInfoRepository = {
+    new DelegableAuthInfoRepository(passwordInfoDAO)
+  }
+
+  /**
+   * Provides the authenticator service.
+   *
+   * @param signer The signer implementation.
+   * @param crypter The crypter implementation.
+   * @param cookieHeaderEncoding Logic for encoding and decoding `Cookie` and `Set-Cookie` headers.
+   * @param fingerprintGenerator The fingerprint generator implementation.
+   * @param idGenerator The ID generator implementation.
+   * @param configuration The Play configuration.
+   * @param clock The clock instance.
+   * @return The authenticator service.
+   */
+  @Provides
+  def provideAuthenticatorService(
+    @Named("authenticator-signer") signer: Signer,
+    @Named("authenticator-crypter") crypter: Crypter,
+    cookieHeaderEncoding: CookieHeaderEncoding,
+    fingerprintGenerator: FingerprintGenerator,
+    idGenerator: IDGenerator,
+    configuration: Configuration,
+    clock: Clock): AuthenticatorService[CookieAuthenticator] = {
+
+    val config = configuration.underlying.as[CookieAuthenticatorSettings]("silhouette.authenticator")
+    val authenticatorEncoder = new CrypterAuthenticatorEncoder(crypter)
+
+    new CookieAuthenticatorService(config, None, signer, cookieHeaderEncoding, authenticatorEncoder, fingerprintGenerator, idGenerator, clock)
+  }
+
+  /**
+   * Provides the avatar service.
+   *
+   * @param httpLayer The HTTP layer implementation.
+   * @return The avatar service implementation.
+   */
+  @Provides
+  def provideAvatarService(httpLayer: HTTPLayer): AvatarService = new GravatarService(httpLayer)
+
+  /**
+   * Provides the CSRF state item handler.
+   *
+   * @param idGenerator The ID generator implementation.
+   * @param signer The signer implementation.
+   * @param configuration The Play configuration.
+   * @return The CSRF state item implementation.
+   */
+  @Provides
+  def provideCsrfStateItemHandler(
+    idGenerator: IDGenerator,
+    @Named("csrf-state-item-signer") signer: Signer,
+    configuration: Configuration): CsrfStateItemHandler = {
+    val settings = configuration.underlying.as[CsrfStateSettings]("silhouette.csrfStateItemHandler")
+    new CsrfStateItemHandler(settings, idGenerator, signer)
+  }
+
+  /**
+   * Provides the password hasher registry.
+   *
+   * @return The password hasher registry.
+   */
+  @Provides
+  def providePasswordHasherRegistry(): PasswordHasherRegistry = {
+    PasswordHasherRegistry(new BCryptSha256PasswordHasher(), Seq(new BCryptPasswordHasher()))
+  }
+
+  /**
+   * Provides the credentials provider.
+   *
+   * @param authInfoRepository The auth info repository implementation.
+   * @param passwordHasherRegistry The password hasher registry.
+   * @return The credentials provider.
+   */
+  @Provides
+  def provideCredentialsProvider(
+    authInfoRepository: AuthInfoRepository,
+    passwordHasherRegistry: PasswordHasherRegistry): CredentialsProvider = {
+
+    new CredentialsProvider(authInfoRepository, passwordHasherRegistry)
+  }
+}
