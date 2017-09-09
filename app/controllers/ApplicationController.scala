b2a65428fcea5f005d1b5d86988d3288ff4f79b0
YoshinoriN <nayo-msoft@outlook.jp>
2017-09-09 20:40:49 +0900

    Create ApplicationController

---
+++ app/controllers/ApplicationController.scala
@@ -0,0 +1,24 @@
+package app.controllers
+
+import javax.inject.Inject
+
+import scala.concurrent.Future
+import play.api.i18n.I18nSupport
+import play.api.mvc.{ AbstractController, AnyContent, ControllerComponents }
+import com.mohiva.play.silhouette.api.actions.SecuredRequest
+import com.mohiva.play.silhouette.api.{ LogoutEvent, Silhouette }
+import controllers.AssetsFinder
+import utils.auth.DefaultEnv
+
+class ApplicationController @Inject() (
+  components: ControllerComponents,
+  silhouette: Silhouette[DefaultEnv]
+)(
+  implicit
+  assets: AssetsFinder
+) extends AbstractController(components) with I18nSupport {
+
+  def index = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    Future.successful(Ok(views.html.home(request.identity)))
+  }
+}
