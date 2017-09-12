75740fc150090b622bc67830e504218bbbcc42d8
YoshinoriN <nayo-msoft@outlook.jp>
2017-09-12 23:39:44 +0900

    Create LDAP connection and connection pool method

--- app/services/LDAPService.scala
+++ app/services/LDAPService.scala
@@ -14,5 +14,20 @@ trait LDAPService {
   val bindDN = configuration.getString("ldap.bindDN")
   val baseDN = configuration.getString("ldap.baseDN")
   val uidAttributeName = configuration.getString("ldap.uidAttributeName")
+  val initialConnextions = configuration.getInt("ldap.initialConnextions")
+  val maxConnections = configuration.getInt("ldap.maxConnections")
+
+  val connection = new LDAPConnection(host, port)
+
+  val connectionPool: LDAPConnectionPool = {
+    if (ldaps) {
+      //TODO: Have to change create over TLS connection.
+      new LDAPConnectionPool(connection, initialConnextions, maxConnections)
+    } else {
+      new LDAPConnectionPool(connection, initialConnextions, maxConnections)
+    }
+  }
+
+}
 
 }
