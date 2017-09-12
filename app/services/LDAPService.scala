a9f9d1c7e557236b31cb9fea9e38d579b70cf218
YoshinoriN <nayo-msoft@outlook.jp>
2017-09-13 00:45:06 +0900

    Create connection using by defined config user

--- app/services/LDAPService.scala
+++ app/services/LDAPService.scala
@@ -9,13 +9,17 @@ trait LDAPService {
   val host = configuration.getString("ldap.host")
   val port = configuration.getInt("ldap.port")
   val ldaps = configuration.getBoolean("ldap.ldaps")
-  val bindDN = configuration.getString("ldap.bindDN")
   val baseDN = configuration.getString("ldap.baseDN")
+  val bindDN = configuration.getString("ldap.bindDN")
+  val password = configuration.getString("ldap.password")
   val uidAttributeName = configuration.getString("ldap.uidAttributeName")
   val initialConnextions = configuration.getInt("ldap.initialConnextions")
   val maxConnections = configuration.getInt("ldap.maxConnections")
 
-  val connection = new LDAPConnection(host, port)
+  /**
+   * Create connection using by config user.
+   */
+  val connection = new LDAPConnection(host, port, bindDN, password)
 
   val connectionPool: LDAPConnectionPool = {
     if (ldaps) {
