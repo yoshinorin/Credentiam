41ad066a9aec68a7ed21fc316e80f8726fe8aa52
YoshinoriN <nayo-msoft@outlook.jp>
2017-09-14 00:08:28 +0900

    Create case class for ActiveDirectorUser

---
+++ app/models/LDAP/Object.scala
@@ -0,0 +1,10 @@
+//TODO: Add attributeses
+case class ActiveDirectoryUser(
+  cn: String,
+  displayName: String,
+  distinguishedName: String,
+  name: String,
+  sAMAccountName: String,
+  sn: String,
+  userPrincipalName: String
+)
