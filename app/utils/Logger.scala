cd40bd5c054ab555cfe7525257d401e8276948fc
YoshinoriN <nayo-msoft@outlook.jp>
2017-09-20 06:15:22 +0900

    Add logger trait

---
+++ app/utils/Logger.scala
@@ -0,0 +1,13 @@
+package utils
+
+/**
+ * Implement this to get a named logger in scope.
+ */
+trait Logger {
+
+  /**
+   * A named logger instance.
+   */
+  val logger = play.api.Logger(this.getClass)
+
+}
