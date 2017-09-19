7b6def32eb468e4532d0cd4cd8a3023ca54ac2a5
YoshinoriN <nayo-msoft@outlook.jp>
2017-09-20 06:57:11 +0900

    Change logger trait
    * Stop using play.api.Logger
    * Use slf4j Logger
    * Create security maker

--- app/utils/Logger.scala
+++ app/utils/Logger.scala
@@ -1,13 +1,16 @@
 package utils
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
 /**
  * Implement this to get a named logger in scope.
  */
 trait Logger {
 
-  /**
-   * A named logger instance.
-   */
-  val logger = play.api.Logger(this.getClass)
+  val logger = LoggerFactory.getLogger(this.getClass)
+  val securityMaker = MarkerFactory.getMarker("SECURITY")
 
 }
