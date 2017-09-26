4ec33cbc81a89703f295c6956de1374b9458ba9f
YoshinoriN <nayo-msoft@outlook.jp>
2017-09-26 23:11:17 +0900

    Separate LDAPConnection options from LDAPServiceProvider

---
+++ app/services/LDAP/LDAPConnectionProvider.scala
@@ -0,0 +1,76 @@
+package app.services.ldap
+
+import scala.collection.mutable
+
+import com.typesafe.config.ConfigFactory
+import com.unboundid.ldap.sdk._
+
+trait LDAPConnectionProvider {
+
+  val configuration = ConfigFactory.load
+  val host = configuration.getString("ldap.host")
+  val port = configuration.getInt("ldap.port")
+  val ldaps = configuration.getBoolean("ldap.ldaps")
+  val baseDN = configuration.getString("ldap.baseDN")
+  val bindDN = configuration.getString("ldap.bindDN")
+  val password = configuration.getString("ldap.password")
+  val uidAttributeName = configuration.getString("ldap.uidAttributeName")
+  val initialConnextions = configuration.getInt("ldap.initialConnextions")
+  val maxConnections = configuration.getInt("ldap.maxConnections")
+
+  def getConnectionOptions: LDAPConnectionOptions = {
+    val connectionOption = new LDAPConnectionOptions
+    connectionOption.setConnectTimeoutMillis(configuration.getInt("ldap.connectTimeout"))
+    connectionOption.setResponseTimeoutMillis(configuration.getInt("ldap.responseTimeout"))
+    connectionOption.setAbandonOnTimeout(configuration.getBoolean("ldap.abandonOnTimeOut"))
+    connectionOption
+  }
+
+  case class UserConnection(
+    dn: String,
+    connection: LDAPConnection
+  //TODO: Add user's role.
+  )
+
+  /**
+   * Create connection using by config user.
+   * TODO: Support LDAPS
+   */
+  protected val defaultConnection = new LDAPConnection(getConnectionOptions, host, port, bindDN, password)
+
+  /**
+   * The connections store by user.
+   */
+  protected val connections: mutable.HashMap[String, UserConnection] = mutable.HashMap()
+
+  /**
+   * Create connection by users and store it.
+   * TODO: Support LDAPS
+   */
+  def createConnectionByUser(uid: String, dn: String, password: String): Unit = {
+    val connection = UserConnection(dn, new LDAPConnection(getConnectionOptions, host, port, dn, password))
+    connections += (uid -> connection)
+  }
+
+  /**
+   * Remove & close connection from connections store by User.
+   */
+  def removeConnectionByUser(uid: String): Unit = {
+    //TODO: Release connection certainly.
+    connections.get(uid) match {
+      case Some(uc) => {
+        uc.connection.close
+        connections -= uid
+      }
+      case None => None
+    }
+  }
+
+  /**
+   * Get Connection by uid.
+   */
+  def getConnectionByUser(uid: String): Option[UserConnection] = {
+    connections.get(uid)
+  }
+
+}
--- app/services/LDAP/LDAPServiceProvider.scala
+++ app/services/LDAP/LDAPServiceProvider.scala
@@ -1,7 +1,7 @@
 package app.services.ldap
 
-import scala.collection.mutable
 import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 import com.typesafe.config.ConfigFactory
 import com.unboundid.ldap.sdk._
@@ -25,70 +25,7 @@ object LDAPServiceProvider {
 
 }
 
-trait LDAPServiceProvider {
-
-  val configuration = ConfigFactory.load
-  val host = configuration.getString("ldap.host")
-  val port = configuration.getInt("ldap.port")
-  val ldaps = configuration.getBoolean("ldap.ldaps")
-  val baseDN = configuration.getString("ldap.baseDN")
-  val bindDN = configuration.getString("ldap.bindDN")
-  val password = configuration.getString("ldap.password")
-  val uidAttributeName = configuration.getString("ldap.uidAttributeName")
-  val initialConnextions = configuration.getInt("ldap.initialConnextions")
-  val maxConnections = configuration.getInt("ldap.maxConnections")
-
-  val connectionOptions = new LDAPConnectionOptions
-  connectionOptions.setConnectTimeoutMillis(configuration.getInt("ldap.connectTimeout"))
-  connectionOptions.setResponseTimeoutMillis(configuration.getInt("ldap.responseTimeout"))
-  connectionOptions.setAbandonOnTimeout(configuration.getBoolean("ldap.abandonOnTimeOut"))
-
-  case class UserConnection(
-    dn: String,
-    connection: LDAPConnection
-  //TODO: Add user's role.
-  )
-
-  /**
-   * Create connection using by config user.
-   * TODO: Support LDAPS
-   */
-  protected val defaultConnection = new LDAPConnection(connectionOptions, host, port, bindDN, password)
-
-  /**
-   * The connections store by user.
-   */
-  protected val connections: mutable.HashMap[String, UserConnection] = mutable.HashMap()
-
-  /**
-   * Create connection by users and store it.
-   * TODO: Support LDAPS
-   */
-  def createConnectionByUser(uid: String, dn: String, password: String): Unit = {
-    val connection = UserConnection(dn, new LDAPConnection(connectionOptions, host, port, dn, password))
-    connections += (uid -> connection)
-  }
-
-  /**
-   * Remove & close connection from connections store by User.
-   */
-  def removeConnectionByUser(uid: String): Unit = {
-    //TODO: Release connection certainly.
-    connections.get(uid) match {
-      case Some(uc) => {
-        uc.connection.close
-        connections -= uid
-      }
-      case None => None
-    }
-  }
-
-  /**
-   * Get Connection by uid.
-   */
-  def getConnectionByUser(uid: String): Option[UserConnection] = {
-    connections.get(uid)
-  }
+trait LDAPServiceProvider extends LDAPConnectionProvider {
 
   /**
    * User bind with LDAP server.
