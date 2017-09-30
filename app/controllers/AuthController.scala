04cdafbb41714a5e88bc2f41108f27e4a8b017a2
YoshinoriN <nayo-msoft@outlook.jp>
2017-09-30 17:16:07 +0900

    Add message and display it when user logged in

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -79,7 +79,7 @@ class AuthController @Inject() (
               authInfo <- authInfoRepository.add(loginInfo, authInfo)
               authenticator <- silhouette.env.authenticatorService.create(loginInfo)
               value <- silhouette.env.authenticatorService.init(authenticator)
-              result <- silhouette.env.authenticatorService.embed(value, Redirect(routes.ApplicationController.index()))
+              result <- silhouette.env.authenticatorService.embed(value, Redirect(routes.ApplicationController.index()).flashing("success" -> Messages("valid.credentials")))
             } yield {
               silhouette.env.eventBus.publish(LoginEvent(user, request))
               logger.info(securityMaker, s"Authentication Succeeded: ${data.uid}")
