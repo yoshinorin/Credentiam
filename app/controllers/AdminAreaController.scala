1816daa6e38591f0a6200ff6f944d028ce438c61
YoshinoriN <nayo-msoft@outlook.jp>
2017-10-09 00:31:33 +0900

    Create authorization function for ldap server administrator

--- app/controllers/AdminAreaController.scala
+++ app/controllers/AdminAreaController.scala
@@ -3,26 +3,29 @@ package app.controllers
 import javax.inject.Inject
 
 import scala.concurrent.Future
+
 import play.api.i18n.I18nSupport
 import play.api.mvc.{ AbstractController, AnyContent, ControllerComponents }
+
 import com.mohiva.play.silhouette.api.actions.SecuredRequest
 import com.mohiva.play.silhouette.api.{ LogoutEvent, Silhouette }
-import controllers.AssetsFinder
+import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
 
+import controllers.AssetsFinder
 import app.services.ldap.LDAPService
 import utils.auth.DefaultEnv
+import utils.auth.WithAdmin
 
 class AdminAreaController @Inject() (
   components: ControllerComponents,
+  credentialsProvider: CredentialsProvider,
   silhouette: Silhouette[DefaultEnv]
 )(
   implicit
   assets: AssetsFinder
 ) extends AbstractController(components) with I18nSupport {
 
-  //TODO: Only administrator accessible
-  def index = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
-    //TODO: Exception handling
+  def index = silhouette.SecuredAction(WithAdmin[DefaultEnv#A](CredentialsProvider.ID)).async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
     Future.successful(Ok(views.html.admin.index(request.identity)))
   }
 
