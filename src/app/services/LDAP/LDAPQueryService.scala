6428e4d811f3a533e9f7317ff1cbfcbdcc3cf19e
YoshinoriN <nayo-msoft@outlook.jp>
2017-11-10 01:31:00 +0900

    Create LDAPQueryService object

---
+++ src/app/services/LDAP/LDAPQueryService.scala
@@ -0,0 +1,21 @@
+package app.services.ldap
+
+import app.utils.config.LDAPSearchableAttributes
+import app.utils.types.SearchRelations
+
+object LDAPQueryService {
+
+  def relationMatcha(relation: String): SearchRelations = {
+    relation.toLowerCase match {
+      case "ANY" => SearchRelations.ANY
+      case "CONTAINS" => SearchRelations.CONTAINS
+      case "EXCLUDES" => SearchRelations.EXCLUDES
+      case "STARTSWITH" => SearchRelations.STARTSWITH
+      case "ENDSWITH" => SearchRelations.ENDSWITH
+      case "EQUAL" => SearchRelations.EQUAL
+      case "NOTEQUAL" => SearchRelations.NOTEQUAL
+      case _ => SearchRelations.ANY
+    }
+  }
+
+}
