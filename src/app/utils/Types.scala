e490562b2c5e21c14598bc3323acf066e6b03925
YoshinoriN <nayo-msoft@outlook.jp>
2017-11-09 23:55:41 +0900

    Create search relation objects

--- src/app/utils/Types.scala
+++ src/app/utils/Types.scala
@@ -5,3 +5,29 @@ package app.utils.types
  * @param userId
  */
 case class UserId(val value: String) extends AnyVal
+
+/**
+ * Search relations abstract class.
+ */
+sealed abstract class SearchRelations(val name: String)
+
+/**
+ * Search relations for search option.
+ */
+object SearchRelations {
+
+  object ANY extends SearchRelations("any")
+
+  object CONTAINS extends SearchRelations("contains")
+
+  object EXCLUDES extends SearchRelations("excludes")
+
+  object STARTSWITH extends SearchRelations("startswith")
+
+  object ENDSWITH extends SearchRelations("endswith")
+
+  object EQUAL extends SearchRelations("equal")
+
+  object NOTEQUAL extends SearchRelations("notequal")
+
+}
