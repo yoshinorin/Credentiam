da636ece739a804a6f5a8b64a332725e70d2d237
YoshinoriN <nayo-msoft@outlook.jp>
2017-11-15 23:32:00 +0900

    Delete getRelationTypeFromString function

--- src/app/services/LDAP/LDAPQueryService.scala
+++ src/app/services/LDAP/LDAPQueryService.scala
@@ -5,22 +5,6 @@ import app.utils.types.SearchRelations
 
 object LDAPQueryService {
 
-  /**
-   * Get [[SearchRelations]] type from string.
-   *
-   * @param relation Name of relation.
-   * @return [[SearchRelations]]
-   */
-  def getRelationTypeFromString(relation: String): SearchRelations = {
-    relation.toUpperCase match {
-      case "CONTAINS" => SearchRelations.CONTAINS
-      case "EXCLUDES" => SearchRelations.EXCLUDES
-      case "STARTSWITH" => SearchRelations.STARTSWITH
-      case "ENDSWITH" => SearchRelations.ENDSWITH
-      case "EQUAL" => SearchRelations.EQUAL
-      case "NOTEQUAL" => SearchRelations.NOTEQUAL
-      case _ => SearchRelations.ANY
-    }
   }
 
 }
