d28b53617fc4c1af790a09abcfba84b4e53806e8
YoshinoriN <nayo-msoft@outlook.jp>
2017-11-18 00:18:32 +0900

    Create get searchable attributes method

--- src/app/services/LDAP/LDAPQueryService.scala
+++ src/app/services/LDAP/LDAPQueryService.scala
@@ -1,10 +1,23 @@
 package app.services.ldap
 
 import app.utils.config.LDAPSearchableAttributes
-import app.utils.types.SearchRelations
+import app.utils.types.{ LDAPObjectType, SearchRelations }
 
 object LDAPQueryService {
 
+
+  /**
+   * Get searchable ldap attributes by [[LDAPObjectType]].
+   *
+   * @param objectType [[LDAPObjectType]]
+   * @return Attributes name.
+   */
+  private def getSearchableAttributes(objectType: LDAPObjectType): List[String] = {
+    objectType match {
+      case LDAPObjectType.ORGANIZATION => LDAPSearchableAttributes.organization
+      case LDAPObjectType.USER => LDAPSearchableAttributes.user
+      case LDAPObjectType.COMPUTER => LDAPSearchableAttributes.computer
+    }
   }
 
 }
