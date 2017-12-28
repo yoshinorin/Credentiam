5f558e1ea9a9b9f300c019d4b60759c39d94e2f6
YoshinoriN <nayo-msoft@outlook.jp>
2017-12-29 00:27:18 +0900

    Sign out when LDAP connection cache not found

--- src/app/models/User.scala
+++ src/app/models/User.scala
@@ -5,6 +5,8 @@ import scala.concurrent.Future
 import com.mohiva.play.silhouette.api.{ Identity, LoginInfo }
 import app.models.UserIdentify
 import app.models.UserDAOImpl._
+import app.models.ldap.UserConnection
+import app.utils.cache.PlaySyncCacheLayer
 import app.utils.types.UserId
 
 /**
@@ -24,8 +26,14 @@ object UserDAOImpl {
  */
 class UserDAOImpl extends UserDAO {
 
-  def find(loginInfo: LoginInfo) = Future.successful(
-    users.find { case (_, user) => user.loginInfo == loginInfo }.map(_._2))
+  def find(loginInfo: LoginInfo) = {
+    PlaySyncCacheLayer.cache.get[UserConnection](loginInfo.providerKey) match {
+      case Some(uc) => {
+        Future.successful(users.find { case (_, user) => user.loginInfo == loginInfo }.map(_._2))
+      }
+      case None => Future.successful(None)
+    }
+  }
 
   def find(userID: UserId) = Future.successful(users.get(userID))
 
