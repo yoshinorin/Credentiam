621f12a2cf21e3372d6e429982f47cbd1cee3e45
YoshinoriN <nayo-msoft@outlook.jp>
2018-01-16 23:51:53 +0900

    Add comments

--- src/app/utils/auth/LDAPAuth.scala
+++ src/app/utils/auth/LDAPAuth.scala
@@ -10,6 +10,9 @@ import com.mohiva.play.silhouette.api.util.PasswordInfo
 import com.mohiva.play.silhouette.persistence.daos.DelegableAuthInfoDAO
 import app.utils.config.LDAPConfig
 
+/**
+  * This class is unused. Only mock for use Silhouette.
+  */
 class LDAPAuth @Inject() (
   cache: AsyncCacheApi
 ) extends DelegableAuthInfoDAO[PasswordInfo] {
@@ -18,12 +21,10 @@ class LDAPAuth @Inject() (
     cache.set(loginInfo.providerKey, PersistentAuthInfo(loginInfo, authInfo), LDAPConfig.expiryDuration).map(_ => authInfo)
   }
 
-  //TODO: Is this method work well ?
   def save(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
     add(loginInfo, authInfo)
   }
 
-  //TODO: Is this method work well ?
   def update(loginInfo: LoginInfo, authInfo: PasswordInfo): Future[PasswordInfo] = {
     add(loginInfo, authInfo)
   }
@@ -32,7 +33,6 @@ class LDAPAuth @Inject() (
     Future.successful(cache.remove(loginInfo.providerKey).map(_ => loginInfo))
   }
 
-  //FIXME : Exception occur
   def find(loginInfo: LoginInfo): Future[Option[PasswordInfo]] = {
     cache.get(loginInfo.providerKey)
   }
